e3801bcc07165ad35f5bf0de7f34e8eef87fcbe7
Jakhongirbek <jakhonyokubjonov@gmail.com>
2025-08-27 03:30:18 +0900

    Vercel Favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,13 +60,17 @@ export default function RootLayout({
 			<head>
 				<link rel='icon' type='image/jpeg' href='/crown.jpg' />
 				<link rel='icon' type='image/x-icon' href='/favicon.ico' />
+				<link rel='icon' type='image/png' href='/crown.jpg' />
 				<link rel='apple-touch-icon' href='/apple-touch-icon.png' />
+				<link rel='apple-touch-icon' sizes='180x180' href='/crown.jpg' />
 				<link rel='shortcut icon' href='/favicon.ico' />
 				<link rel='manifest' href='/manifest.json' />
 				<meta name='theme-color' content='#1f2937' />
 				<meta name='apple-mobile-web-app-capable' content='yes' />
 				<meta name='apple-mobile-web-app-status-bar-style' content='default' />
 				<meta name='apple-mobile-web-app-title' content='King Kebab' />
+				<meta name='msapplication-TileColor' content='#1f2937' />
+				<meta name='msapplication-TileImage' content='/crown.jpg' />
 			</head>
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
